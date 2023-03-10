789fa32a87062ef165d03443fc3517a7b48c5ccc
hori19 <fourbasic19@gmail.com>
2023-03-10 14:29:28 +0900

    【rails】リレーションシップ用のモデルとルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
-  get 'favorites/create'
-  get 'favorites/destroy'
-
-  root 'tweets#index'
-
+  
+  root 'tweets#index' 
 
   devise_for :users
+
   resources :tweets do
-    resource :favorite, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
-
-  resources :users  # 追加
-  
+  # ================ここをネスト(入れ子)した形に変更
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
+  end
+  #======================================
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
